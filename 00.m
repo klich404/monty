985dd35786684f717a09dfbfe48c355958279bfc
klich404 <klich4010@gmail.com>
2021-05-13 18:39:38 -0500

    error line handle

--- 00.m
+++ 00.m
@@ -1,1 +1,4 @@
-selfjghdug
+push 1
+push 2
+push 3
+pall
--- main.c
+++ main.c
@@ -10,8 +10,10 @@
 int main(int argc, char *argv[])
 {
 	char *buffer = NULL, *filename = NULL, **lines = NULL, **opcodes = NULL;
+	char *cmd[] = {"push", "pall"};
 	stack_t *head = NULL;
-	unsigned int x = 0;
+	unsigned int x = 0, j = 0;
+	int status = -1;
 
 	if (argc != 2)
 	{
@@ -27,7 +29,22 @@ int main(int argc, char *argv[])
 		opcodes = tokenizer(lines[x], " ");
 		if (opcodes[1])
 			value = atoi(opcodes[1]);
-		mont_func(opcodes[0], &head, (x + 1));
+		while (cmd[j])
+		{
+			if (opcodes[0] == cmd[j])
+			{
+				mont_func(opcodes[0], &head, (x + 1));
+				status = 1;
+				printf("-------------\n");
+				break;
+			}
+			j++;
+		}
+		if (status != 1)
+		{
+			printf("Llines: unknown instruction array");
+			exit (EXIT_FAILURE);
+		}
 		x++;
 	}
 	free(opcodes);
